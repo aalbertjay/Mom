9119d0762a95def7775d9723c48876bdb2ae3954
SLoh4137 <sloh4137@gmail.com>
2016-11-12 04:40:09 -0500

    Allow for localhost to run in Visual Studio

--- js/index.js
+++ js/index.js
@@ -1,3 +1,11 @@
+var http = require('http');
+var port = process.env.port || 1337;
+http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello World\n');
+}).listen(port);
+
+
 var login = require("facebook-chat-api");
 
 // Create simple echo bot
@@ -9,23 +17,23 @@ var schedule = require('node-schedule');
 var rule = new schedule.RecurrenceRule();
 rule.seconds = 10;
 
-login({email: "litreesofgreaterwashington@gmail.com", password: "davidarthi"}, function callback (err, api) {
-  var j = schedule.scheduleJob(rule, function(){
-
-    if(err)
-      return console.error(err);
-
-    var momID = 100000225248771;
-
-    api.getUserID("Jeremy Du", function(err, data) {
-      if(err) return callback(err);
-
-      // Send the message to the best match (best by Facebook's criteria)
-      var threadID = data[0].userID;
-      var msg = {body: "Hey mom, how are you today?"};
-      api.sendMessage(msg, threadID);
+login({ email: "litreesofgreaterwashington@gmail.com", password: "davidarthi" }, function callback(err, api) {
+    var j = schedule.scheduleJob(rule, function () {
+        
+        if (err)
+            return console.error(err);
+        
+        var momID = 100000225248771;
+        
+        api.getUserID("Stephan Loh", function (err, data) {
+            if (err) return callback(err);
+            
+            // Send the message to the best match (best by Facebook's criteria)
+            var threadID = data[0].userID;
+            var msg = { body: "Hey mom, how are you today?" };
+            api.sendMessage(msg, threadID);
 
       //api.sendMessage(msg, momID);
+        });
     });
-  });
 });
